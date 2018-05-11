a2a8cf2d185eb379c84f25162ac31788cebfa3ff
Екатерина Вакурова <VakurovaEA@mgpu.ru>
2018-05-11 11:05:45 +0300

    Laba4

--- Laba4/Program.cs
+++ Laba4/Program.cs
@@ -6,10 +6,47 @@ using System.Threading.Tasks;
 
 namespace Laba4
 {
-    class Program
+    public class A
     {
-        static void Main(string[] args)
+        private int a = 27;
+        private int b = 9;
+        public int c
         {
+            get
+            {
+                if (a > b) return a;
+                else return b;
+            }
+            set
+            {
+                c = value;
+            }
+
+        }
+        public class B : A
+        {
+            private int d = 5;
+
+            public int c2
+            {
+                get
+                {
+                    return d %= 3;
+                }
+            }
+        }
+        class Programm
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Вакурова Екатерина.Лабораторная 4");
+
+                A obj1 = new A();
+                Console.WriteLine("  1: " + obj1.c);
+                B obj2 = new B();
+                Console.WriteLine("  2: " + obj2.c2);
+                Console.ReadKey();
+            }
         }
     }
 }
